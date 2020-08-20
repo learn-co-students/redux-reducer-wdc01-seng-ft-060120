7d2fd3923ac0a1fa889a7d6a153e988634e46371
steve <stevepesce879@gmail.com>
2020-08-20 15:01:57 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  const presents = 
+  {
+    INCREASE: {numberOfPresents: state.numberOfPresents + 1}
+  }
+  const newState = presents[action.type]
+  return newState ? newState : state
 }
